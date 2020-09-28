68404b2dc5763ddc444a1738de44724bbbeed682
Jagriti <jagriti.agni@gmail.com>
2020-09-29 01:52:56 +0530

    [Jagriti]ADDED: Welcome msg

---
+++ src/com/bridgelabz/user/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.user;
+
+public class UserRegistration {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the user registration system");
+		
+	}
+}
