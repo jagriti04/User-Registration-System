47d522c8a3c594c216b608cca43fc184adb358f4
Jagriti <jagriti.agni@gmail.com>
2020-09-29 18:17:57 +0530

    [Jagriti]ADDED: checks all the emails given

--- src/com/bridgelabz/user/UserRegistration.java
+++ src/com/bridgelabz/user/UserRegistration.java
@@ -5,6 +5,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	public static final String[] emailArray =  {"abc", "abc@yahoo.com", "abc-100@yahoo.com", 
+			"abc111@abc.com","abc.100@yahoo.com","abc-100@abc.net", "abc.100@abc.com.au","abc@1.com", 
+			"abc@gmail.com.com", "abc+100@gmail.com", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com",
+			"abc()*@gmail.com", "abc@%*.com","abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", 
+			"abc@gmail.com.aa.au", "abc@.com.my",  }; 
 	
 	public void validateName(String name) {
 		String patternString = "(^[A-Z])[A-Za-z]{2,}$";
@@ -23,6 +28,14 @@ public class UserRegistration {
 	public void validatePassword(String password) {
 		String patternString = "^(?=.*\\d)(?=[^!@#$%^&*()_+=]*[!@#$%^&*()_+=][^!@#$%^&*()_+=]*$)(?=.*[A-Z]).{8,}$";
 		regexMatch(password, patternString);
+	}
+	public void checkValidEmailAll() {
+		for(String matchString : emailArray) {
+			String patternString = "^[a-zA-z]{1}([.]{0,1}[a-zA-z0-9+-]{1,}){0,}[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-z]{2,3}([.]{1}[a-z]{2}){0,1}$";
+			System.out.println("match email: " + matchString);
+		    regexMatch(matchString, patternString);
+		    		
+		}
 	}
 	
 	public static void regexMatch(String matchContent, String patternString) {  	
@@ -43,24 +56,27 @@ public class UserRegistration {
 		System.out.println("Welcome to the user registration system");
 		
 		UserRegistration userReg = new UserRegistration();
-//		System.out.println("Enter the first name");
-//		String fName = sc.nextLine();
-//		userReg.validateName(fName);
-//		
-//		System.out.println("Enter the last name");
-//		String lName = sc.nextLine();
-//		userReg.validateName(lName);
-//		
-//		System.out.println("Enter the email");
-//		String email = sc.nextLine();
-//		userReg.validateEmail(email);
-//		
-//		System.out.println("Enter the mobile num.");
-//		String mobileNum = sc.nextLine();
-//		userReg.validateMobile(mobileNum);
+		System.out.println("Enter the first name");
+		String fName = sc.nextLine();
+		userReg.validateName(fName);
+		
+		System.out.println("Enter the last name");
+		String lName = sc.nextLine();
+		userReg.validateName(lName);
+		
+		System.out.println("Enter the email");
+		String email = sc.nextLine();
+		userReg.validateEmail(email);
+		
+		System.out.println("Enter the mobile num.");
+		String mobileNum = sc.nextLine();
+		userReg.validateMobile(mobileNum);
 		
 		System.out.println("Enter the password");
 		String password = sc.nextLine();
 		userReg.validatePassword(password);
+		
+		System.out.println("------ Checking all Email ------- ");
+		userReg.checkValidEmailAll();
 	}
 }
